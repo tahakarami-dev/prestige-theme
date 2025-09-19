a7a96e3dac42bcfcccff9559dbb4aba4c732ec80
Taha Karami <tahakarami2088@gamil.com>
2025-09-19 19:04:35 +0330

    feat: Developing the theme settings panel

--- inc/prestige-settings.php
+++ inc/prestige-settings.php
@@ -6,25 +6,72 @@ if (class_exists('CSF')) {
     $prefix = 'prestige_settings';
 
     CSF::createOptions($prefix, array(
-        'menu_title' => 'پرستیژ',
+        'menu_title' => 'تنظیمات پرستیژ',
         'menu_slug'  => 'prestige-settings',
         'menu_icon'  => PRESTIGE_THEMEURL . 'assets/images/navigation.svg'
     ));
 
     CSF::createSection($prefix, array(
-        'title'  => 'Tab Title 1',
-        'fields' => array(
-            array(
-                'id'    => 'opt-text',
-                'type'  => 'text',
-                'title' => 'Simple Text',
-            ),
-        )
+        'title'  => 'عمومی',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'هدر',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'فوتر',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'وبلاگ',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'نمونه کار',
+        'fields' => array()
+    )); 
+
+    CSF::createSection($prefix, array(
+        'title'  => 'فروشگاه',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'شبکه‌های اجتماعی',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'سئو',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'عملکرد',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => '404',
+        'fields' => array()
+    ));
+
+    CSF::createSection($prefix, array(
+        'title'  => 'پیشرفته',
+        'fields' => array()
     ));
 }
+
 
 function prestige_settings($key = '')
 {
     $options = get_option('prestige_settings');
     return isset($options[$key]) ? $options[$key] : null;
 }
+
+
